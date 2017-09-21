2b8561b30ca2c640bf7fd529c90159e73a52cedd
Krishna Kumar <kks32@cam.ac.uk>
2017-09-21 06:37:53 +0100

    :calendar: :construction: Booking dates

--- controller/dates.go
+++ controller/dates.go
@@ -1,9 +1,24 @@
+// package bookingdates
 package main
 
-import "fmt"
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+	// "github.com/labstack/echo"
+	// "net/http"
+)
 
+type Date struct {
+    // BookingDate
+    BookingDate string `json:"bookingdate,omitempty"`
+}
+
+// func bookingDates(c echo.Context) error {
 func main() {
+	var bookingdates []Date
+	// Errors
+	var err error
 	// Start date as tomorrow
 	startdate := time.Now().Local().AddDate(0, 0, 1)
 	fmt.Println("Start date: ", startdate.Format("2006-01-02"))
@@ -16,6 +31,15 @@ func main() {
 		// Exclude weekends (0 - Sunday, 6 - Saturday)
 		if d.Weekday() != 0 {
 			fmt.Println("Dates: ", d.Format("2006-01-02"), d.Weekday())
+			bdate := Date{BookingDate: d.Format("2006-01-02")}
+			fmt.Println(bdate)
+			bookingdates = append(bookingdates, bdate)
 		}
+	}	
+	dates, err := json.Marshal(bookingdates)
+	if err != nil {
+		fmt.Println("JSON marshaling failed: %s", err)
 	}
+	fmt.Printf("%s\n", dates)
+	//return c.JSON(http.StatusCreated, user)
 }
