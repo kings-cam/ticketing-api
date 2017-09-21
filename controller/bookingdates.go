72f11c36663d646fcb669cc1976d245828f3ede9
Krishna Kumar <kks32@cam.ac.uk>
2017-09-21 14:27:50 +0100

    :fire: Remove unused lines

--- controller/bookingdates.go
+++ controller/bookingdates.go
@@ -2,7 +2,6 @@ package bookingdates
 
 import (
 	// "encoding/json"
-	"fmt"
 	"time"
 	"github.com/labstack/echo"
 	"net/http"
@@ -12,15 +11,12 @@ var bookingdates []string
 
 // Return dates of bookings
 func BookingDates(c echo.Context) error {
-	// Errors
-	// var err error
 	// Start date as tomorrow
 	startdate := time.Now().Local().AddDate(0, 0, 1)
-	fmt.Println("Start date: ", startdate.Format("2006-01-02"))
-	// End date as 3 months from tomorrow
-	enddate := startdate.AddDate(0, 2, 0)
-	fmt.Println("End date: ", enddate.Format("2006-01-02"))
 
+	// End date as 3 months from tomorrow
+	enddate := startdate.AddDate(0, 3, 0)
+	
 	// Iterate over dates to print all allowed dates
 	for d := startdate; d != enddate; d = d.AddDate(0, 0, 1) {
 		// Exclude weekends (0 - Sunday, 6 - Saturday)
@@ -28,12 +24,5 @@ func BookingDates(c echo.Context) error {
 			bookingdates = append(bookingdates, d.Format("2006-01-02"))
 		}
 	}
-	/*
-	dates, err := json.Marshal(bookingdates)
-	if err != nil {
-		fmt.Println("JSON marshaling failed: %s", err)
-	}
-	fmt.Printf("%s\n", dates)
-        */
 	return c.JSON(http.StatusCreated, bookingdates)
 }
