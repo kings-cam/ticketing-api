a51bc613736754f3f72ce4c0c12e9d67d4b8ee50
Krishna Kumar <kks32@cam.ac.uk>
2017-09-23 17:15:55 +0100

    :computer: Server file

---
+++ cmd/api/server.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	// Routes
+	"tickets"
+	// Echo framework
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const port string = ":4000"
+
+// Server API
+func main() {
+	e := echo.New()
+
+	// Middleware
+	// Server log
+	e.Use(middleware.Logger())
+	// Recovers from panics anywhere in the chain
+	e.Use(middleware.Recover())
+
+	// CORS for cross-domain access controls
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
+
+	// Initialise routes
+	tickets.Routes(e)
+
+	// Server
+	e.Logger.Fatal(e.Start(port))
+}
