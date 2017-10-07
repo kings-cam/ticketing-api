e5c1c2cd8f37c6407582810a1841c31b583b80c4
Krishna Kumar <kks32@cam.ac.uk>
2017-10-07 10:35:02 -0700

    :art: Refactor routes

--- cmd/api.go
+++ cmd/api.go
@@ -4,93 +4,38 @@ import (
 	// Ticketing package
 	"tickets"
 
-	"encoding/json"
 	"net/http"
 	"log"
 	"time"
 
-	// CORS
-	"github.com/rs/cors"
 	// Mongodb
 	"gopkg.in/mgo.v2"
-	// Gorilla Mux
-	"github.com/gorilla/mux"
-	// Negroni framework
-	"github.com/urfave/negroni"
-	// Stats
-	"github.com/thoas/stats"
 )
 
 const port string = ":4000"
 
-// Server API
+// main Runs the tickets api server
 func main() {
-	apirouter := mux.NewRouter()
-
-	// Stats middleware
-	statsmw := stats.New()
-
-	// CORS for cross-domain access controls
-	corsmw := cors.New(cors.Options{
-		AllowedOrigins: []string{"*"},
-		AllowedMethods: []string{"GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"},
-
-	})
-
-	/*
-	//For production, keep HTTPSProtection = true
-	HTTPSProtection := false
-	if HTTPSProtection {
-		app.Use(restgate.New("X-Auth-Key", "X-Auth-Secret", restgate.Static, restgate.Config{HTTPSProtectionOff: false, Key: []string{c.API_ENDPOINT_KEY}, Secret: []string{c.API_ENDPOINT_SECRET}}))
-        */
+	// Get router
+	apirouter := tickets.Router()
 	
 	// Create database and session
 	db := tickets.DB{}
 	session, err := db.Dial()
-
+	
 	if err != nil {
 		panic(err)
 	}
 
 	defer session.Close()
 	session.SetMode(mgo.Monotonic, true)
-	
-	// API Router
-	apirouter.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Ticketing API\n"))
-	})
-	
-	
-	// API V1 router
-	apiv1router := mux.NewRouter().PathPrefix("/api/v1").Subrouter().StrictSlash(true)
-	apirouter.PathPrefix("/api/v1").Handler(negroni.New(
-		negroni.NewRecovery(),
-		negroni.NewLogger(),
-		statsmw,
-		corsmw,
-		negroni.Wrap(apiv1router),
-	))
 
-	// API version 1.0 welcome
-	apiv1router.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Ticketing API version 1!\n"))
-	})
-	
-	// Stats
-	apiv1router.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		stats := statsmw.Data()
-		b, _ := json.Marshal(stats)
-		w.Write(b)
-	})
-        
+	// Ensure Index
+	// tickets.EnsureIndex(session)
 
-	// Booking dates
-	apiv1router.HandleFunc("/dates", tickets.BookingDates(session)).Methods("GET")
-
-	// Config Booking dates
-	apiv1router.HandleFunc("/dates/config", tickets.ConfigBookingDates(session)).Methods("POST")
-
+	// Create API v1 routes
+	apiv1router := tickets.V1Router(apirouter)
+	tickets.Routes(apiv1router, session)
 
 	// Create and launch server
 	log.Println("Launching web api in http://localhost"+port)
@@ -102,5 +47,5 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}	
 	log.Fatal(server.ListenAndServe())
-}
 
+}
---
+++ routes.go
@@ -0,0 +1,98 @@
+package tickets
+
+import (
+	"encoding/json"
+	"net/http"
+
+	// CORS
+	"github.com/rs/cors"
+	// JWT
+	// "github.com/dgrijalva/jwt-go"
+	// JSON Web Tokens middleware Auth0
+	// "github.com/auth0/go-jwt-middleware"
+	// Mongodb
+	"gopkg.in/mgo.v2"
+	// Gorilla Mux
+	"github.com/gorilla/mux"
+	// Negroni framework
+	"github.com/urfave/negroni"
+	// Stats
+	"github.com/thoas/stats"
+)
+
+
+// Router Returns the mux api router with CORS, stats and logging
+func Router() *mux.Router {
+	apirouter := mux.NewRouter()
+	
+	/*
+	//For production, keep HTTPSProtection = true
+	HTTPSProtection := false
+	if HTTPSProtection {
+		app.Use(restgate.New("X-Auth-Key", "X-Auth-Secret", restgate.Static, restgate.Config{HTTPSProtectionOff: false, Key: []string{c.API_ENDPOINT_KEY}, Secret: []string{c.API_ENDPOINT_SECRET}}))
+        */
+		
+	// API Router
+	apirouter.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ticketing API\n"))
+	})
+        
+    return apirouter
+}
+
+func V1Router(apirouter *mux.Router) *mux.Router {
+	// Stats middleware
+	statsmw := stats.New()
+
+	// CORS for cross-domain access controls
+	corsmw := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"},
+
+	})
+
+/*
+	// Auth0 JWT middleware
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("dsAREIWkSHee604VTbq4kJf0imEeWwdC"), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+	})
+*/
+	
+	// API V1 router
+	apiv1router := mux.NewRouter().PathPrefix("/api/v1").Subrouter().StrictSlash(true)
+	apirouter.PathPrefix("/api/v1").Handler(negroni.New(
+		negroni.NewRecovery(),
+		negroni.NewLogger(),
+		// negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		statsmw,
+		corsmw,
+		negroni.Wrap(apiv1router),
+	))
+
+	// Stats
+	apiv1router.HandleFunc("/stats", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		stats := statsmw.Data()
+		b, _ := json.Marshal(stats)
+		w.Write(b)
+	})
+
+	return apiv1router
+}
+
+// Routes define API version 1.0  router for tickets package
+func Routes(apiv1router *mux.Router, session *mgo.Session) {
+	// API version 1.0 welcome
+	apiv1router.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Ticketing API version 1!\n"))
+	})
+
+	// Booking dates
+	apiv1router.HandleFunc("/dates", BookingDates(session)).Methods("GET")
+
+	// Config Booking dates
+	apiv1router.HandleFunc("/dates/config", ConfigBookingDates(session)).Methods("POST")
+}
