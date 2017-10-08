6534ce88d07ff7ea066ea878214a72998b04b29d
Krishna Kumar <kks32@cam.ac.uk>
2017-10-07 21:37:23 -0700

    :ticket: Add pricing to tickets

---
+++ pricing.go
@@ -0,0 +1,73 @@
+package tickets
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	// Mongo DB
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Pricing struct {
+	ID int `json:"id"`
+	// Adult ticket price
+	AdultPrice float32 `json:"adultprice"`
+	// Child ticket price
+	ChildPrice float32 `json:"childprice"`
+	// Concession ticket price
+	ConcessionPrice float32 `json:"concessionprice"`
+	// Guidebook price
+	GuidePrice float32 `json:"guideprice"`
+	// Guide book laguages
+	GuideBooks []string `json:"guidebooks"`
+}
+
+
+// ConfitPricing assigns ticket prices
+func ConfitPricing(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+
+		var price Pricing
+
+		// Decode POST JSON file
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&price)
+		if err != nil {
+			ErrorWithJSON(w, "Incorrect body", http.StatusBadRequest)
+			return
+		}
+
+		// Open price collection
+		c := session.DB("tickets").C("pricing")
+
+		// Try to update if price is found
+		err = c.Update(bson.M{"id": 0}, &price)
+		if err != nil {
+			switch err {
+			default:
+				ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+				log.Println("Failed to update price: ", err)
+				return
+			// Price is not present, do an insert
+			case mgo.ErrNotFound:
+				log.Println("Price not present, creating a new price")
+				err = c.Insert(&price)
+
+				if err != nil {
+					ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+					log.Println("Failed to insert price: ", err)
+					return
+				}
+			}
+		}
+
+		// Write response
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Location", r.URL.Path + "/0")
+		w.WriteHeader(http.StatusCreated)
+	}
+}
--- routes.go
+++ routes.go
@@ -93,10 +93,12 @@ func Routes(apiv1router *mux.Router, session *mgo.Session) {
 	// Booking dates
 	apiv1router.HandleFunc("/dates", BookingDates(session)).Methods("GET")
 
-	// Config Booking dates
-	apiv1router.HandleFunc("/config/dates", ConfigBookingDates(session)).Methods("POST")
-
 	// Get Session
 	apiv1router.HandleFunc("/sessions/{date}", BookingSessions(session)).Methods("GET")
 
+	// Config Booking dates
+	apiv1router.HandleFunc("/config/dates", ConfigBookingDates(session)).Methods("POST")
+
+	// Config Booking dates
+	apiv1router.HandleFunc("/config/pricing", ConfitPricing(session)).Methods("POST")
 }
