d8d88a580b035601e90eb9fe3e698f548940a6c6
Krishna Kumar <kks32@cam.ac.uk>
2018-01-26 06:45:50 +0000

    :email: Add inline email

--- mail.go
+++ mail.go
@@ -19,9 +19,9 @@ func sendmail(booking *Booking) {
 		/* Subject*/ "King's College Chapel Ticket",
 		/* Body */ "",
 		/* Recipient */ booking.Email)
-	message.SetHtml("<html><p>Booking reference: " + booking.UUID + "<br/>Booking date and sessions: " + booking.Date + " " + booking.Session + "<br/>Total for " + strconv.Itoa(booking.Ntickets) + " tickets (" + strconv.Itoa(booking.NAdults) + " adults, " + strconv.Itoa(booking.Nconcession) + " students and " + strconv.Itoa(booking.Nchild) + " child) and " + strconv.Itoa(booking.Nguides) + "guides is: <span>&#163;</span>" + fmt.Sprintf("%.2f", booking.Total) + "</p><p>Whilst at King's, do visit the King's College Visitor Centre on King's Parade (opposite the main gate) for souvenirs, gifts, CDs and clothing. On buy online  at <a href=\"http://shop.kings.cam.ac.uk\">http://shop.kings.cam.ac.uk</a></p></html>")
+	message.SetHtml("<html><p>Booking reference: " + booking.UUID + "<br/>Booking date and sessions: " + booking.Date + " " + booking.Session + "<br/>Total for " + strconv.Itoa(booking.Ntickets) + " tickets (" + strconv.Itoa(booking.NAdults) + " adults, " + strconv.Itoa(booking.Nconcession) + " students and " + strconv.Itoa(booking.Nchild) + " child) and " + strconv.Itoa(booking.Nguides) + " guides is: <span>&#163;</span>" + fmt.Sprintf("%.2f", booking.Total) + "</p><img src=\"cid:" + booking.UUID + ".png\"><p>Whilst at King's, do visit the King's College Visitor Centre on King's Parade (opposite the main gate) for souvenirs, gifts, CDs and clothing. On buy online  at <a href=\"http://shop.kings.cam.ac.uk\">http://shop.kings.cam.ac.uk</a></p></html>")
 
-	message.AddAttachment(booking.UUID + ".png")
+	message.AddInline(booking.UUID + ".png")
 	resp, id, err := mg.Send(message)
 	if err != nil {
 		log.Fatal(err)
